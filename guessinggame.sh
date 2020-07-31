79949e295affa77b4edf348dfe7ab4e511b5587a
ssiiriisshhaa <69040844+ssiiriisshhaa@users.noreply.github.com>
2020-07-31 08:45:38 -0400

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,21 @@
-
+echo "Guessing game"
+echo"lets guess number of files in directory"
+wcount=$(ls-la |wc-l)
+function qn {
+echo "enter number o ffiles in repository "
+read num
+}
+while [[$num -ne $wcount]]
+do
+qn
+if[[$num -gt $wcount]]
+then
+echo"you have guessed great number of files in dirctory"
+elif [[$num -it $wcount]]
+then
+echo"you have guessed less number of files in diretory"
+else
+echo"you have wrongly geussed number of files in diretory"
+fi
+done
+echo"the game of guessing directoryis almost done "
